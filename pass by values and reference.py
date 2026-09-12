d0a42b7d60029e1e2fcc6e17740613ab87a75870
Richa Makwana <richamakwana13@gmail.com>
2026-09-13 01:02:29 +0530

    learned diff. btwn call by value and reference

--- pass by values and reference.py
+++ pass by values and reference.py
@@ -1,22 +1,21 @@
-#pass by values 
+# call by value 
+import copy
+def function(x):
+  x+=1
+  print(f"Inside the function = {x}")
 
-def func(num):
-  return num+num
+num = 12 #num is immutable so only it's valuewill goes to the function not the adderess itself 
+print(f"Outside the function = {num}")
+function(num)
 
-def func2(lst):
-  lst.append(100)
-  return lst
+# call by reference
+
+def fun2(num):
+  num = copy.deepcopy(num)
+  num.append(100)
+  print(f"Inside the function = {num}")
 
 
-
-num = 4 #integers are not mutable 
-lst = [1, 2, 3] #list are mutable
-
-value = func(num)
-print(value)
-print(num)
-value2 = func2(lst)
-print(value2)
-print(lst)
-
-# whatever antities are muatble that can be changed by function and immutable elements can not be changed
+element = [1, 2, 3] # element is mutable type so here it's address will go to the function 
+fun2(element)
+print(f"Outside the function = {element}")
