79ae57c6bf0e84a05b465f6e2222192307ebf7d7
Richa Makwana <richamakwana13@gmail.com>
2026-09-13 01:45:45 +0530

    changes saved

--- 4_mutating.py
+++ 4_mutating.py
@@ -2,6 +2,7 @@
 
 def rebind(x):
   x.append(100)
+  # here i am changing something within the taken variable so it will change both as it muatble. so it's called as mutating
   print(f"Inside the function = {x}")
 
 nums = [1, 2, 3, 4]
